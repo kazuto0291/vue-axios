3d439b661f35f8af40fa7bd40ada6372a2058281
kazuto0291 <zutozuto03170291@gmail.com>
2020-12-30 21:14:47 +0900

    リフレッシュトークン
    idTokenを更新する。一時間ごとに再帰的にリフレッシュトークンを発行する

--- src/store/index.js
+++ src/store/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import Vuex from 'vuex';
 import axios from '../axios-auth';
 import router from '../router';
+import axiosRefresh from '../axios-refresh';
 
 Vue.use(Vuex);
 
@@ -18,7 +19,7 @@ export default new Vuex.Store({
     }
   },
   actions: {
-    login({ commit }, authData) {
+    login({ commit, dispatch }, authData) {
       axios.post(
         'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDtRVsU0Scbfku9UkUFbDoRoeTopnSdFb8',
         {
@@ -28,8 +29,24 @@ export default new Vuex.Store({
         }
       ).then(response => {
         commit('updateIdToken', response.data.idToken);
+        setTimeout(() => {
+          dispatch('refreshIdToken', response.data.refreshToken);
+        }, response.data.expiresIn * 1000);
         router.push('/')
         console.log(response);
+      })
+    },
+    refreshIdToken({ commit, dispatch }, refreshToken) {
+      axiosRefresh.post('/token?key=AIzaSyDtRVsU0Scbfku9UkUFbDoRoeTopnSdFb8',
+        {
+          grant_type: 'refresh_token',
+          refresh_token: refreshToken
+        }
+      ).then(response => {
+        commit('updateIdToken', response.data.id_token);
+        setTimeout(() => {
+          dispatch('refreshIdToken', response.data.refresh_token)
+        }, response.data.expires_in * 1000);
       })
     },
     register({ commit }, authData) {
